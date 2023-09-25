7dabd6dd9c6629e09441f9a9158471f12f730492
PekkoMoreto <pedromoreto666@gmail.com>
2023-09-25 14:02:58 -0300

    Initial commit

---
+++ src/test/java/dio/rest/ApiRestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
